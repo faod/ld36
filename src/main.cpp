b61e2ed7c16d53d4383a7a6bec8ac52f10f59a46
Medioni Thomas <medioni.thomas@gmail.com>
2014-06-13 04:47:41 +0200

    support c++11

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <memory>
 
 int main(int argc, char *argv[])
 {
 	std::cout << argc << " " << argv[0] << std::endl;
+
+	std::unique_ptr<int> p(new int);
 	return 0;
 }
