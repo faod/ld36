0e745075eddaa68bedf8c713b346b96e33b7be77
Thomas Medioni <medioni.thomas@gmail.com>
2016-08-25 03:47:17 +0200

    correct launch and unexpected runtime exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <game/Game.h>
 
+#include <iostream>
+#include <exception>
+
 int main(int argc, char *argv[])
 {
 	(void) argc, (void) argv;
 
-	faod::Game g("ludum", 800, 600);
-	g.run();
+    try
+    {   
+	    faod::Game g("ludum", 800, 600);
+        try 
+        {
+            //If the game has loaded successfully
+            g.run();
+        } catch(std::exception& e)
+        {
+            std::cerr << "Game has unexpectedly failed: " << e.what() << std::endl;
+        }
+
+    } catch(std::exception& e)
+    {
+        std::cerr << "Failure to launch the game: " << e.what() << std::endl;
+    }
 
 	return 0;
 }
