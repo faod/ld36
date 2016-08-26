d9641ae7cd97faf431a3a53004227c17e78c4911
Thomas Medioni <medioni.thomas@gmail.com>
2016-08-26 03:27:31 +0200

    add CollidableObjects

---
+++ include/objects/CollidableObject.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <objects/Object.hpp>
+#include <objects/ConvexHull.hpp>
+
+namespace faod
+{
+    class CollidableObject : public Object
+    {
+        public:
+            CollidableObject(const CollidableObject &o) = delete;
+            CollidableObject(CollidableObject &&o)      = delete;
+            CollidableObject& operator=(const CollidableObject &o) = delete;
+            CollidableObject& operator=(CollidableObject &&o) = delete;
+
+
+            explicit CollidableObject(const ConvexHull &convexHull);
+            
+            //true if the objects are currently colliding
+            bool isColliding(const CollidableObject& other) const;
+    
+            //returns the convexHull for this object with proper translation rotation and scaling
+            ConvexHull getConvexHull() const;
+
+            //returns the axis aligned bounding box for this object (x, y, w, h)
+            glm::vec4 getBoundingBox() const;
+            
+        private:
+            virtual void    updateCurrent(sf::Time delta);
+        
+        private:
+            ConvexHull convexHull_;
+        
+    };
+}
---
+++ src/objects/CollidableObject.cpp
@@ -0,0 +1,39 @@
+#include <objects/CollidableObject.hpp>
+
+namespace faod
+{
+    CollidableObject::CollidableObject(const ConvexHull& convexHull) : Object(), convexHull_(convexHull)
+    {
+    }
+    
+    void    CollidableObject::updateCurrent(sf::Time delta)
+    {
+        (void) delta;
+        //FIXME 
+        //
+        //Check if movement will produce collision
+        //if not call parent::updateCurrent(delta)
+    }
+
+    bool CollidableObject::isColliding(const CollidableObject& other) const
+    {
+        const ConvexHull c1 = getConvexHull();
+        const ConvexHull c2 = other.getConvexHull();
+
+        return (c1.intersects(c2) || c2.intersects(c1));
+        
+    }
+
+    ConvexHull CollidableObject::getConvexHull() const
+    {
+        ConvexHull c = convexHull_;
+        c.translate(glm::vec2(getPosition().x, getPosition().y));
+        c.rotate(-((getRotation() / 360) * 2 * PI));
+        return c;
+    }
+
+    glm::vec4 CollidableObject::getBoundingBox() const
+    {
+        return getConvexHull().getBoundingBox();
+    }
+}
