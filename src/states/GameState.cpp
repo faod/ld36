3966eeedaecc6f60fe8c2bf1e68408da725fe81f
Thomas Medioni <medioni.thomas@gmail.com>
2016-08-27 11:43:51 +0200

    add gamestate template impl

--- src/states/GameState.cpp
+++ src/states/GameState.cpp
@@ -1,5 +1,27 @@
 #include <states/GameState.hpp>
+#include <game/World.hpp>
 
 namespace faod
 {
+    GameState::GameState(StateStack &stack, Context context) : State(stack, context) , world_(new World(*context.window_)), playerController_(*context.playerController_)
+    {
+    }
+
+    void GameState::draw()
+    {
+        //TODO
+    }
+
+    bool GameState::update(sf::Time delta)
+    {
+        (void) delta;
+        return false;
+        //TODO
+    }
+    bool GameState::handleEvent(const sf::Event& event)
+    {
+        (void) event;
+        return false;
+        //TODO
+    }
 }
