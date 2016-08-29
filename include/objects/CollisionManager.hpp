4953dadffea5843d5c374c022dc862582ec36ae0
Thomas Medioni <medioni.thomas@gmail.com>
2016-08-29 21:59:41 +0200

    add collision for catapult

--- include/objects/CollisionManager.hpp
+++ include/objects/CollisionManager.hpp
@@ -24,5 +24,18 @@ namespace faod
             std::vector<std::weak_ptr<CollidableObject>> collidables_;
             std::map<faod::Collision::Type, Collision> collisions_;
     };
+    
+    template <typename Collidable, typename Function>
+    std::function<void(CollidableObject*, CollidableObject&)> derivedCollision(Function fn)
+    {
+        return [=] (CollidableObject* object, CollidableObject& other)
+        {
+            //attempt to cast
+            assert(dynamic_cast<Collidable*>(object) != nullptr);
+
+            //Downcast
+            fn(static_cast<Collidable*>(object), other);
+        };
+    }
 
 }
--- src/objects/CollisionManager.cpp
+++ src/objects/CollisionManager.cpp
@@ -1,10 +1,17 @@
 #include <objects/CollisionManager.hpp>
 
+#include <objects/Catapult.hpp>
+
 namespace faod
 {
     CollisionManager::CollisionManager()
     {
         //add collisions to the map
+        Collision c;
+        c.mask = faod::Collision::Type::Bonus;
+        c.action = derivedCollision<Catapult>
+            (std::bind(&Catapult::collideWith, std::placeholders::_1, std::placeholders::_2));
+        collisions_[faod::Collision::Type::Catapult] = c;
     }
     void CollisionManager::collisionCheck(CollidableObject& object)
     {
@@ -20,7 +27,7 @@ namespace faod
             {
                 continue;
             }
-            if(ptr->getCollisionType() & mask)
+            if(ptr->getCollisionType() != faod::Collision::Type::None && ptr->getCollisionType() & mask)
             {
                 //able to collide
                 if(object.isColliding(*ptr))
