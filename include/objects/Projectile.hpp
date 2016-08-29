069a99168e401562ca6665ac776c503cdf303281
Thomas Medioni <medioni.thomas@gmail.com>
2016-08-29 22:27:11 +0200

    projectile category & update (to not call collidableobject's

--- include/objects/Projectile.hpp
+++ include/objects/Projectile.hpp
@@ -12,10 +12,15 @@ namespace faod
             Projectile(sf::Texture &texture, int startw, int starth, float weight, int textx, int texty);
             void launch(sf::Vector2f dir, float force);
 
+            //virtual Collision::Type getCollisionType() const;
+        protected:
+            bool launched_;
+
+        private:        
+            virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+            virtual void updateCurrent(sf::Time delta);
         private:
             float weight_;
-            virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
-            //graphics
             sf::Texture     &texture_;
             sf::Sprite       sprite_;
     };
@@ -24,12 +29,16 @@ namespace faod
     {
         public:
             Pumpkin(sf::Texture &texture, int startw, int starth);
+
+            virtual Collision::Type getCollisionType() const;
     };
 
     class Rock : public Projectile
     {
         public:
             Rock(sf::Texture &texture, int startw, int starth);
+
+            virtual Collision::Type getCollisionType() const;
     };
 
 }
--- src/objects/Projectile.cpp
+++ src/objects/Projectile.cpp
@@ -1,4 +1,5 @@
 #include <objects/Projectile.hpp>
+#include <objects/Catapult.hpp>
 
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Rect.hpp>
@@ -8,6 +9,7 @@ namespace faod
         :CollidableObject(ConvexHull::boxHull(glm::vec2(16., 16.)))
         ,weight_(weight)
         ,texture_(texture)
+        ,launched_(false)
     {
         sprite_.setTexture(texture_);
         sprite_.setTextureRect(sf::IntRect(textx,texty,16,16));
@@ -19,6 +21,11 @@ namespace faod
     {
     }
     
+    void Projectile::updateCurrent(sf::Time delta)
+    {
+
+    }
+
     void Projectile::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const
     {
         target.draw(sprite_, states);
@@ -28,10 +35,35 @@ namespace faod
         :Projectile(texture, startw, starth, 10., 0, 240)
     {
     }
+    Collision::Type Pumpkin::getCollisionType() const
+    {
+        SceneNode *dummy = nullptr;
+
+
+        dummy = dynamic_cast<Catapult*>(parent_);
+        if(dummy == nullptr && !launched_)
+            return Collision::Type::Bonus;
+        if(dummy == nullptr && launched_)
+            return Collision::Type::Projectile; 
+        
+        return Collision::Type::None;
+    }
 
     Rock::Rock(sf::Texture &texture, int startw, int starth)
         :Projectile(texture, startw, starth, 50., 64, 240)
     {
+    }
+    Collision::Type Rock::getCollisionType() const
+    {
+        SceneNode *dummy = nullptr;
+
+        dummy = dynamic_cast<Catapult*>(parent_);
+        if(dummy == nullptr && !launched_)
+            return Collision::Type::Bonus;
+        if(dummy == nullptr && launched_)
+            return Collision::Type::Projectile; 
+        
+        return Collision::Type::None;
     }
 
 }
